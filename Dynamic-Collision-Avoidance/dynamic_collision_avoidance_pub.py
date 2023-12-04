ee125565d76cf3deb522376454ab419d3dce2aef
Nick Zarilla <nickzarilla@gmail.com>
2023-12-03 20:05:38 -0500

    Add files via upload
    Switched to ServoMsg; changed speed

---
+++ Dynamic-Collision-Avoidance/dynamic_collision_avoidance_pub.py
@@ -0,0 +1,143 @@
+#!/usr/bin/env python3
+import rclpy
+from rclpy.node import Node
+from sensor_msgs.msg import LaserScan
+from deepracer_interfaces_pkg.msg import ServoCtrlMsg
+import time
+from std_msgs.msg import String
+import keyboard
+
+
+class MinimalPublisher(Node):
+
+	def __init__(self):
+		super().__init__('minimal_publisher')
+		self.publisher_ = self.create_publisher(Twist, '/cmd_vel', 10)
+		self.i = 0
+		self.speed_mod = .5
+	
+	def car_go(self):
+		# ServoCtrlMsg publish
+		msg = ServoCtrlMsg()
+		msg.angle = 0.0
+		msg.throttle = self.speed_mod
+		self.publisher_servo.publish(msg)
+		self.get_logger().info('Publishing: "%s"' % msg)
+		self.i += 1
+
+		# self.get_logger().info('Publishing: "%s"' % msg)
+
+	def car_stop(self):
+		# ServoCtrlMsg publish
+		msg = ServoCtrlMsg()
+		msg.angle = 0.0
+		msg.throttle = 0.0
+		self.publisher_servo.publish(msg)
+		self.get_logger().info('Publishing: "%s"' % msg)
+		self.i += 1
+		cont = input("Paused: Enter to continue...")
+
+		# self.get_logger().info('Publishing: "%s"' % msg)
+
+	def car_steer_left(self):
+
+		if keyboard.is_pressed('t') and keyboard.is_pressed('x'):
+			print('oh no')
+			self.car_stop()
+			#stop_sign_subscriber.destroy_node()
+			rclpy.shutdown()
+
+		for i in range(0,100):
+
+			if keyboard.is_pressed('t') and keyboard.is_pressed('x'):
+				print('oh no')
+				self.car_stop()
+				#stop_sign_subscriber.destroy_node()
+				rclpy.shutdown()
+
+			msg = ServoCtrlMsg()
+			msg.throttle = self.speed_mod * .75
+			msg.angle = 1.0*(100/(i+1))
+			self.publisher_servo.publish(msg)
+			self.get_logger().info('Publishing: "%s"' % msg)
+		time.sleep(1)
+
+		for i in range(0,100):
+
+			if keyboard.is_pressed('t') and keyboard.is_pressed('x'):
+				print('oh no')
+				self.car_stop()
+				#stop_sign_subscriber.destroy_node()
+				rclpy.shutdown()
+
+			msg = ServoCtrlMsg()
+			msg.throttle = self.speed_mod * .75
+			msg.angle = -1.0*(100/(i+1))
+			self.publisher_servo.publish(msg)
+			self.get_logger().info('Publishing: "%s"' % msg)
+		time.sleep(1)
+		self.i += 1
+		msg = ServoCtrlMsg()
+		msg.throttle = self.speed_mod * .75
+		msg.angle = 0.0
+		self.publisher_servo.publish(msg)
+		self.get_logger().info('Publishing: "%s"' % msg)
+		self.i += 1
+		#time.sleep(5)
+
+
+	def car_steer_right(self):
+
+		if keyboard.is_pressed('t') and keyboard.is_pressed('x'):
+			print('oh no')
+			self.car_stop()
+			#stop_sign_subscriber.destroy_node()
+			rclpy.shutdown()
+
+		for i in range(0,100):
+			
+			if keyboard.is_pressed('t') and keyboard.is_pressed('x'):
+				print('oh no')
+				self.car_stop()
+				#stop_sign_subscriber.destroy_node()
+				rclpy.shutdown()
+			
+			msg = ServoCtrlMsg()
+			msg.throttle = self.speed_mod * .75
+			msg.angle = -1.0*(100/(i+1))
+			self.publisher_servo.publish(msg)
+			self.get_logger().info('Publishing: "%s"' % msg)
+		time.sleep(1)
+		for i in range(0,100):
+
+			if keyboard.is_pressed('t') and keyboard.is_pressed('x'):
+				print('oh no')
+				self.car_stop()
+				#stop_sign_subscriber.destroy_node()
+				rclpy.shutdown()
+
+			msg = ServoCtrlMsg()
+			msg.throttle = self.speed_mod * .75
+			msg.angle = 1.0*(100/(i+1))
+			self.publisher_servo.publish(msg)
+			self.get_logger().info('Publishing: "%s"' % msg)
+		time.sleep(1)
+		msg = ServoCtrlMsg()
+		msg.throttle = self.speed_mod * .75
+		msg.angle = 0.0
+		self.publisher_servo.publish(msg)
+		self.get_logger().info('Publishing: "%s"' % msg)
+		self.i += 1
+		# time.sleep(5)
+ 
+
+def main(args=None):
+	rclpy.init(args=args) 
+	minimal_publisher = MinimalPublisher()   
+	rclpy.spin(minimal_publisher)   
+	minimal_publisher.destroy_node()
+	rclpy.shutdown()
+
+
+if __name__ == '__main__':
+	main()
